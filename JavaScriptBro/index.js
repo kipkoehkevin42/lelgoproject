575f70f95855657e5cfd66ffab086a6032753ff9
unknown <kipkoechkevin42@gmail.com>
2023-03-10 13:08:54 +0300

    style push

---
+++ JavaScriptBro/index.js
@@ -0,0 +1,21 @@
+/* let age = window.prompt("How old are you");
+
+console.log(typeof age);
+age = Number(age);
+console.log(typeof age);
+age += 1;
+
+console.log("Happy birthday! You are", age, "years old");
+*/
+
+let x;
+let y;
+let z;
+
+x = Number ("3.12");
+x = String ("3.12");
+x = Boolean ("pizza");
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
